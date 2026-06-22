9c1cd8bee804344325f64f8a42af7e4ca4dfa885
Neha Vetal <nehavetal520@gmail.com>
2026-06-22 22:07:25 +0530

    config updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("Trying MongoDB connection...");
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected:", conn.connection.host);
   } catch (error) {
-    console.error("DB Connection Failed:", error);
+    console.error("DB Connection Failed:", error.message);
     process.exit(1);
   }
 };
